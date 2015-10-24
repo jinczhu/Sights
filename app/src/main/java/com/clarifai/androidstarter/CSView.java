d47f7529741495a9439f70bf50e229e6754f41c2
Shivansh Singh <pro.shivansh@gmail.com>
2015-10-24 14:35:26 -0400

    Screen Orientation Fix

--- app/src/main/java/com/clarifai/androidstarter/CSView.java
+++ app/src/main/java/com/clarifai/androidstarter/CSView.java
@@ -1,13 +1,22 @@
 package com.clarifai.androidstarter;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.hardware.Camera;
+import android.view.Window;
+import android.view.WindowManager;
+
+import static android.content.Context.*;
 
 public class CSView extends SurfaceView implements SurfaceHolder.Callback {
+    private static final String TAG = RecognitionActivity.class.getSimpleName();
     private SurfaceHolder cHolder;
     public Camera camera;
+    public boolean isPreviewRunning;
 
     public CSView(Context context, Camera mcamera){
         super(context);
@@ -16,22 +25,60 @@ public class CSView extends SurfaceView implements SurfaceHolder.Callback {
         cHolder.addCallback(this);
     }
 
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
-        if(cHolder.getSurface() == null){
-            return;
-        }
-
-        try{
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
+    {
+        if (isPreviewRunning)
+        {
             camera.stopPreview();
-        }catch (Exception e){
-
         }
 
-        try{
+        Camera.Parameters parameters = camera.getParameters();
+
+        Context mContext = getContext();
+
+        WindowManager win;
+        String wanted = Context.WINDOW_SERVICE;
+        win = (WindowManager)mContext.getSystemService(wanted);
+
+        Display display = win.getDefaultDisplay();
+
+        if(display.getRotation() == Surface.ROTATION_0)
+        {
+            parameters.setPreviewSize(height, width);
+            camera.setDisplayOrientation(90);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_90)
+        {
+            parameters.setPreviewSize(width, height);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_180)
+        {
+            parameters.setPreviewSize(height, width);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_270)
+        {
+            parameters.setPreviewSize(width, height);
+            camera.setDisplayOrientation(180);
+        }
+
+        camera.setParameters(parameters);
+        previewCamera();
+    }
+
+    public void previewCamera()
+    {
+        try
+        {
             camera.setPreviewDisplay(cHolder);
             camera.startPreview();
-        }catch(Exception e) {
-
+            isPreviewRunning = true;
+        }
+        catch(Exception e)
+        {
+            Log.d(TAG, "Cannot start preview", e);
         }
     }
 
